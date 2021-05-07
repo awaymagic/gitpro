f7f6f30d481156aab5b1061513dda168bc36c80c
guowei <694094705@qq.com>
2021-05-07 17:34:46 +0800

    IDEA中的Test.java

---
+++ src/main/java/com/away/test/test.java
@@ -0,0 +1,12 @@
+package com.away.test;
+
+/**
+ * Date:2021/5/7
+ * Author:away
+ * Description:
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("HelloGit");
+    }
+}
